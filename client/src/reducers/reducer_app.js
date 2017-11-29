8afbe37bd3d87b33bf4b4cc0940d5b460b9fc810
Cecilia Goss <ceci.goss@gmail.com>
2017-11-29 16:45:56 -0600

    working on redux

--- client/src/reducers/reducer_app.js
+++ client/src/reducers/reducer_app.js
@@ -2,3 +2,16 @@ const initialState = {
   counter: 0,
   string: ''
 };
+
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return Object.assign({}, state, {
+        
+      })
+
+  }
+
+
+}
