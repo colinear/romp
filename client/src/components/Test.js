c586063964b3f63a80a003c5a56116bb8e4414c1
Cecilia Goss <ceci.goss@gmail.com>
2017-11-28 19:28:31 -0600

    added homer simpson and greeting

---
+++ client/src/components/Test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import homerSimpson from '../assets/homer-simpson.svg';
+import '../styles/Test.css';
+
+export default class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={homerSimpson} className="homer-simpson" alt="logo" />
+        Hello! HELLOOOO
+      </div>
+    );
+  }
+}
