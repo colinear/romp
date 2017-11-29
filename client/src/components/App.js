abc970d7c1935b859bddedff2409ce68782c7e8a
Cecilia Goss <ceci.goss@gmail.com>
2017-11-28 19:42:19 -0600

    add twitch key files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         </header>
         <Test />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edityttttttttttt <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
---
+++ server/twitch.example.js
@@ -0,0 +1,1 @@
+module.exports = 'CLIENT ID GOES HERE';
