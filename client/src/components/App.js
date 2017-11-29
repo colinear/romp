aa89b474be29d73c2e42ecc89ac45fcd3f8d065d
Cecilia Goss <ceci.goss@gmail.com>
2017-11-29 09:33:29 -0600

    playing around with flexbox grid

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,36 @@
+// Libraries
 import React, { Component } from 'react';
-import Test from './Test.js';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+// Components
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+// Styles
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Test />
-        <p className="App-intro">
-          To get started, edityttttttttttt <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Grid fluid>
+          <Row className="App-photos">
+            <Col xs={12} md={3}>
+              <img src="https://lh4.googleusercontent.com/-OowXWkgMSHI/AAAAAAAAAAI/AAAAAAAAANE/rOf2DCA2AXo/photo.jpg" />
+            </Col>
+            <Col xs={12} md={3}>
+              <img src="https://i.pinimg.com/736x/ee/65/02/ee65020f6b7491b4c73ebf2b1abb20c5--the-heavens-lightning.jpg" />
+            </Col>
+            <Col xs={12} md={3}>
+              <img src="https://lh5.googleusercontent.com/-5J5sg8zM-xI/AAAAAAAAAAI/AAAAAAABqdY/CAwXvPSSST8/photo.jpg" />
+            </Col>
+            <Col xs={12} md={3}>
+              <img src="https://lh5.googleusercontent.com/-W0hkrrMAl3A/AAAAAAAAAAI/AAAAAAAAEHk/MG5aFF2cEB8/photo.jpg" />
+            </Col>
+          </Row>
+        </Grid>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = props => {
+  return (
+    <footer className="Footer">
+      <h1>This is the footer.</h1>
+    </footer>
+  );
+};
+
+
+export default Footer;
