06a3c88a0fc962a3bcbdbb8cf3264683e1e16b38
ccm36 <cameron.c.malloy@gmail.com>
2017-11-28 19:05:04 -0600

    Connect mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var path = require('path');
 var app = express()
+var mongoose = require('mongoose');
+const MONGODB_URI = require('../db/mongo.js');
+mongoose.connect(MONGODB_URI);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(path.join(__dirname, '../client/build')))
