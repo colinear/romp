f3573a1bc89798bad65246ef753975374c462796
Clayton Smart <claysmart@gmail.com>
2017-11-29 15:51:28 -0600

    Working on user schema/models

--- server/api/models/User.js
+++ server/api/models/User.js
@@ -1,13 +1,12 @@
-var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../schema/User.js');
 
 // user method for generating a hashed password
-userSchema.methods.generateHash = function(password) {
+User.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // user method to check if password is valid
-userSchema.methods.validPassword = function(password) {
+User.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
 }
