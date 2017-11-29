eee6466bc00ad08ee8c2bc64b6f91ea3ecf2b9f5
Clayton Smart <claysmart@gmail.com>
2017-11-29 16:55:29 -0600

    Moved bcrypt methods back to schema (#15)
    * Working on schema/model
    
    * Working on user schema/models
    
    * Moved bcrypt methods back to schema

--- server/api/models/User.js
+++ server/api/models/User.js
@@ -1,16 +1,3 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-var userSchema = require('../schema/User.js');
+var User = require('../../../db/schema/User.js');
 
 // user method for generating a hashed password
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// user method to check if password is valid
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-}
-
-var User = mongoose.model('User', userSchema);
-module.exports = User;
