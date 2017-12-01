0df20aef881e1146aadc42d1be33da51c97e523c
ccm36 <cameron.c.malloy@gmail.com>
2017-12-01 12:11:26 -0600

    Solve error when no user exists

--- server/api/models/User.js
+++ server/api/models/User.js
@@ -2,10 +2,33 @@ var UserSchema = require('../../../db/schema/User.js');
 
 var User = {};
 
+User.getUser = (username) => {
+  return UserSchema.find({'username': username}).exec((err, user) => {
+    if (err) {throw err}
+
+    // returns empty array
+    if (user.length <= 0) {
+      console.log('USER DOES NOT EXIST');
+    }
+    return user;
+  });
+};
+
+
+//get all users
+//We won't need this for our app, but good for testing db
+User.getAllUsers = () => {
+  return UserSchema.find({}).exec((err, users) => {
+    if (err) {throw err}
+    return users;
+  });
+};
+
+
 User.createUser = (username, password, email, firstName, lastName, profilePicURL) => {
 
   //create new user
-  var newUser = new UserSchema ();
+  var newUser = new UserSchema();
   newUser.username = username;
   newUser.password = newUser.generateHash(password);
   newUser.email = email;
@@ -22,20 +45,6 @@ User.createUser = (username, password, email, firstName, lastName, profilePicURL
     } else {
       console.log('user added')
     }
-  });
-};
-
-User.getUser = (username) => {
-  return UserSchema.find({'username': username}).exec((err, user) => {
-    return user;
-  });
-};
-
-//get all users
-//We won't need this for our app, but good for testing db
-User.getAllUsers = () => {
-  return UserSchema.find({}).exec((err, users) => {
-    return users;
   });
 };
 
