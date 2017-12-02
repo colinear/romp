89f5a2743789f915386d9083cada2d9d4f32a6ee
Clayton Smart <claysmart@gmail.com>
2017-12-02 11:54:07 -0600

    Change game schema

--- db/schema/Game.js
+++ db/schema/Game.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var gameSchema = new Schema({
-  game: {__TWITCH API STUFF__},
+  name: {type: String},
 });
 
 var Game = mongoose.model('events', userSchema);
