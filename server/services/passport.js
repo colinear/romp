25f0ad440abd1dc0a16aa6dd9e6920a6b36b3919
ccm36 <cameron.c.malloy@gmail.com>
2017-12-06 16:37:10 -0600

    Add twitch api for authenticated login

--- server/services/passport.js
+++ server/services/passport.js
@@ -4,12 +4,13 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
+const TwitchStrategy = require('passport-twitch').Strategy;
 
 const localLogin = new LocalStrategy({}, (username, password, done) => {
   User.findOne({username}, (err, user) => {
     if (err) return done(err);
     if (!user) return done(null, false);
-    
+
     user.validPassword(password, (err, isMatch) => {
       if (err) return done(err);
       if (!isMatch) return done(null, false);
@@ -34,5 +35,23 @@ const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
   })
 })
 
+const twitchOptions = {
+  clientID: config.TWITCH_CLIENT_ID,
+  clientSecret: config.TWITCH_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/twitch/callback",
+  scope: "user_read"
+}
+
+const twitchLogin = new TwitchStrategy(twitchOptions, (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({ twitchId: profile.id }, (err, user) => {
+      return done(err, user);
+    });
+  }
+)
+
+passport.serializeUser(function(user, done) { done(null, user) });
+passport.deserializeUser(function(user, done) { done(null, user) });
+
 passport.use(jwtLogin);
 passport.use(localLogin);
+passport.use(twitchLogin);
