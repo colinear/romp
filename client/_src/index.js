10b7086287ce33f91eb30d6c29b731fe8136b871
Cecilia Goss <ceci.goss@gmail.com>
2017-12-06 11:22:33 -0600

    Utilizing code and migrating over

---
+++ client/_src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import rootReducer from './reducers/index.js';
+
+// let store = createStore(rootReducer);
+let store = applyMiddleware(ReduxPromise)(createStore)(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,4 @@
-// TODO: Put action constants in here.
+export const AUTH_USER = 'auth_user';
+export const UNAUTH_USER = 'unauth_user';
+export const AUTH_ERROR = 'auth_error';
+export const FETCH_MESSAGE = 'fetch_message';
