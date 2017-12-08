d2ca81e77e524a0b7de561584cce40cf710a1c1a
ccm36 <cameron.c.malloy@gmail.com>
2017-12-08 12:58:11 -0600

    Migrate api call from front end to back end [in progress]

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { IGDB_KEY } from '../config'
 import { browserHistory } from 'react-router';
 import {
   AUTH_USER,
@@ -11,7 +12,6 @@ import {
 } from './types';
 
 const ROOT_URL = 'http://localhost:3001'; // Server URL
-const IGDB_API = 'https://api-2445582011268.apicast.io';
 
 export function loginUser(userData) {
   return function(dispatch) {
@@ -106,8 +106,9 @@ export function getEvent(eventID, callback) {
 
 export function getGames(numOfGames = 12, callback) {
   return function(dispatch) {
-    axios.get(`${IGDB_API}/games/?fields=name,popularity&order=popularity:desc`)
+    axios.get(`${ROOT_URL}/games/?fields=name,popularity&order=popularity:desc`)
     .then((games) => {
+      console.log('games in actions: ', games)
       dispatch ({ type: GET_GAMES, games });
       callback();
     })
--- server/api/route.js
+++ server/api/route.js
@@ -2,10 +2,48 @@ const router = require('express').Router();
 const helpers = require('./helpers.js');
 const passportService = require('../services/passport');
 const passport = require('passport');
+const config = require('../config');
+console.log('config : ', config)
+const axios = require('axios');
+
+const igdb = require('igdb-api-node').default;
+const client = igdb(config.IGDB_KEY);
+
+const IGDB_API = 'https://api-2445582011268.apicast.io';
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 // const requireTwitchLogin = passport.authenticate('twitch', { failureRedirect: '/' })
+
+let desiredFields = [
+  'id', 'name', 'slug', 'url', 'summary',
+  'popularity', 'total_rating', 'developers', 
+  'category', 'keywords', 'genres', 'first_release_date', 
+  'screenshots', 'videos', 'cover', 'esrb', 'websites',
+  'tags', 
+];
+
+router.get('/games', (req, res) => {
+  console.log('params in routes at /games: ', req.query, config.IGDB_KEY)
+  // axios.get(`${IGDB_API}/games`, {
+  //   headers: {
+  //     'user-key': config.IGDB_KEY,
+  //     'Accept': 'application/json'
+  //   }
+  // })
+  client.games({
+    fields : '*',
+    limit: 12,
+  })
+    .then(games => {
+      console.log('games: ', games)
+      res.json(games)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(401).send({ err });
+    })
+})
 
 
 router.post('/signup', helpers.createUser);
@@ -85,6 +123,5 @@ router.get('/event/:eventid', (req, res) => {
     res.end(JSON.stringify(event));
   });
 })
-// Create a route that gets the event.
 
 module.exports = router;
