e7ebb1d0ca5ea4c098740ea0b9380c5f8563ac62
Cecilia Goss <ceci.goss@gmail.com>
2017-12-13 19:23:53 -0600

    Image hover parallax component works.

--- client/src/components/MouseHoverParallaxImage.js
+++ client/src/components/MouseHoverParallaxImage.js
@@ -9,13 +9,40 @@ export default class MouseHoverParallaxImage extends React.Component {
         x: 0,
         y: 0
       }
-    }
+    };
+    this.style = {
+      background: (img, x, y) => {
+        let width = 290;
+        let height = 400;
+        let widthPercent = x / 290 * 100;
+        let heightPercent = y / 400 * 100;
+        if (x && y) {
+          return {
+            backgroundImage: `url("${img}")`,
+            backgroundSize: 'auto',
+            backgroundPositionX: `${widthPercent}%`,
+            backgroundPositionY: `${heightPercent}%`,
+            height: '400px',
+            width: '100%'
+          }
+        } else {
+          return {
+            backgroundImage: `url("${img}")`,
+            backgroundSize: 'cover',
+            height: '400px',
+            width: '100%'
+          }
+        }
+      }
+    };
   }
 
   handleMouseHover = (e) => {
+      var bounds = e.target.getBoundingClientRect();
+      console.log(e);
       console.log('Hovered!');
-      let x = e.clientX;
-      let y  = e.clientY;
+      let x = e.clientX - bounds.left;
+      let y  = e.clientY - bounds.top;
       console.log(x, y);
       this.setState({hover: true, position: {x, y}});
   }
@@ -28,36 +55,10 @@ export default class MouseHoverParallaxImage extends React.Component {
     let componentStyle;
     console.log(this.state);
     if (this.state.hover) {
-      componentStyle = style.background(this.props.background, this.state.position.x, this.state.position.y);
+      componentStyle = this.style.background(this.props.background, this.state.position.x, this.state.position.y);
     } else {
-      componentStyle = style.background(this.props.background);
+      componentStyle = this.style.background(this.props.background);
     }
     return <div style={componentStyle} onMouseMove={this.handleMouseHover} onMouseOut={this.handleMouseExit}></div>
   }
 }
-
-const style = {
-  background: (img, x, y) => {
-    let width = 290;
-    let height = 400;
-    let widthPercent = x / 290 * 100;
-    let heightPercent = y / 290 * 100;
-    if (x && y) {
-      return {
-        backgroundImage: `url("${img}")`,
-        backgroundSize: 'auto',
-        backgroundPositionX: `${widthPercent * 0.5}%`,
-        backgroundPositionY: `${heightPercent * 0.5}%`,
-        height: '400px',
-        width: '100%'
-      }
-    } else {
-      return {
-        backgroundImage: `url("${img}")`,
-        backgroundSize: 'cover',
-        height: '400px',
-        width: '100%'
-      }
-    }
-  }
-};
