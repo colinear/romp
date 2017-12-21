811c5830e4623303928131454c7ef35be06f3b78
Clayton Smart <claysmart@gmail.com>
2017-12-21 10:18:29 -0600

    Sort events by upcoming, future, and past

--- client/src/components/EventGrid.js
+++ client/src/components/EventGrid.js
@@ -8,11 +8,30 @@ import moment from 'moment';
 const EventGrid = props => {
   let time = moment().format('MMMM Do YYYY, h:mm a');
   let now = JSON.stringify(time);
+  let upcoming = JSON.stringify(moment().add(3, 'days').format('MMMM Do YYYY, h:mm a'));
+  console.log('TEST', JSON.stringify(props.events[12].eventAt) < upcoming, props.events[16], now, upcoming);
   return (
     <div>
       <h2 style={{textAlign: "center", marginTop: "1%"}}>Upcoming Events</h2>
       <div className="ui link cards" style={{justifyContent: 'center', marginTop: "1%"}}>
-      {props.events.filter(event => {return now < JSON.stringify(event.eventAt)})
+      {props.events.filter(event => {return JSON.stringify(event.eventAt) < upcoming && JSON.stringify(event.eventAt) > now})
+        .map((event, index) => {
+          console.log('event', event, index);
+          return (
+            <EventCard
+              key={event._id}
+              index={index}
+              event={event}
+              onClick={() => {
+                browserHistory.push(`/events/${event._id}`);
+              }}
+            />
+          )
+      })}
+      </div>
+      <h2 style={{textAlign: "center", width: "90%", margin: "2% auto 0", paddingTop: "1%", borderTop: "1px solid LightGrey"}}>Future Events</h2>
+      <div className="ui link cards" style={{justifyContent: 'center', marginTop: "1%"}}>
+      {props.events.filter(event => {return upcoming < JSON.stringify(event.eventAt)})
         .map((event, index) => {
           console.log('event', event, index);
           return (
@@ -27,7 +46,7 @@ const EventGrid = props => {
           );
         })}
       </div>
-    <h2 style={{textAlign: "center", width: "90%", position: "center", margin: "2% auto", paddingTop: "1%", borderTop: "1px solid LightGrey"}}>Past Events</h2>
+    <h2 style={{textAlign: "center", width: "90%", margin: "2% auto 0", paddingTop: "1%", borderTop: "1px solid LightGrey"}}>Past Events</h2>
     <div className="ui link cards" style={{justifyContent: 'center', marginTop: "1%"}}>
     {props.events.filter(event => {return now > JSON.stringify(event.eventAt)})
       .map((event, index) => {
