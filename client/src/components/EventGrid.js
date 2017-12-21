fcc51c4c916a4437f2a4249be1849c0bc878e854
Clayton Smart <claysmart@gmail.com>
2017-12-20 18:58:57 -0600

    Sort the event page into upcoming and past events

--- client/src/components/EventGrid.js
+++ client/src/components/EventGrid.js
@@ -3,12 +3,34 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import EventCard from './EventCard.js';
 import { Link, browserHistory } from 'react-router';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 const EventGrid = props => {
-  console.log('event grid', props);
+  let time = moment().format('MMMM Do YYYY, h:mm a');
+  let now = JSON.stringify(time);
   return (
+    <div>
+      <h2 style={{textAlign: "center", marginTop: "1%"}}>Upcoming Events</h2>
+      <div className="ui link cards" style={{justifyContent: 'center', marginTop: "1%"}}>
+      {props.events.filter(event => {return now < JSON.stringify(event.eventAt)})
+        .map((event, index) => {
+          console.log('event', event, index);
+          return (
+            <EventCard
+              key={event._id}
+              index={index}
+              event={event}
+              onClick={() => {
+                browserHistory.push(`/events/${event._id}`);
+              }}
+            />
+          );
+        })}
+      </div>
+    <h2 style={{textAlign: "center", width: "90%", position: "center", margin: "2% auto", paddingTop: "1%", borderTop: "1px solid LightGrey"}}>Past Events</h2>
     <div className="ui link cards" style={{justifyContent: 'center', marginTop: "1%"}}>
-      {props.events.map((event, index) => {
+    {props.events.filter(event => {return now > JSON.stringify(event.eventAt)})
+      .map((event, index) => {
         console.log('event', event, index);
         return (
           <EventCard
@@ -22,6 +44,7 @@ const EventGrid = props => {
         );
       })}
     </div>
+  </div>
   );
 };
 
